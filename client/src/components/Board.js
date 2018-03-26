e5ee8b4d14a90e6036f45263cd4ee9e138df4d97
Bobby Caya <bob.caya1@gmail.com>
2018-03-26 11:16:39 -0600

    basic dice styles

---
+++ client/src/components/Board.js
@@ -0,0 +1,17 @@
+import React from 'react' 
+import Dice from './Dice'
+
+const Board = () => (
+  <div>
+    <Dice value={1} />
+    <Dice value={2} />
+    <Dice value={3} />
+    <Dice value={4} />
+    <Dice value={5} />
+    <Dice value={6} />
+
+    
+  </div>
+)
+
+export default Board; 
